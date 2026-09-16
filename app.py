f3b0d9beebcb4bd5339d0463ffb7feda1919c6e2
free-raffle <garcia.albert@gmail.com>
2026-09-16 09:46:50 +0200

    Add gunicorn and support dynamic PORT for cloud deployment (Render)

--- app.py
+++ app.py
@@ -467,4 +467,5 @@ def get_point_details(point_idx):
 if __name__ == '__main__':
     if not CSV_READY:
         CSV_READY = load_data_into_cache()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
